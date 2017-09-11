c688ad873baf8b0b34c91703fc33f489ef0dea1b
Giampaolo Rodola <g.rodola@gmail.com>
2017-09-11 15:49:41 +0800

    experiments

--- config.sh
+++ config.sh
@@ -20,7 +20,7 @@ function run_tests {
         pip install $DEPENDS
     fi
     python --version
-    cd ..
-    PSUTIL_TESTING=1 PYTHONWARNINGS=all python psutil/tests/__main__.py
+    pwd
+    PSUTIL_TESTING=1 python -Wa ../psutil/psutil/tests/__main__.py
     # PSUTIL_TESTING=1 python -Wa ../psutil/psutil/tests/test_memory_leaks.py
 }
