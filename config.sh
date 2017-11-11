52b5a292d585788b0b776222722a87e388984d0c
Giampaolo Rodola <g.rodola@gmail.com>
2017-11-11 10:54:08 +0100

    progress

--- config.sh
+++ config.sh
@@ -4,8 +4,6 @@ if [ "$MB_PYTHON_VERSION" == '2.6' ]; then
     DEPENDS="ipaddress unittest2 argparse mock==1.0.1"
 elif [ $(lex_ver $MB_PYTHON_VERSION) -le $(lex_ver 3.2) ]; then
     DEPENDS="ipaddress mock"
-elif [ "$MB_PYTHON_VERSION" == '3.3' ]; then
-    DEPENDS=ipaddress
 fi
 
 function pre_build {
@@ -21,9 +19,7 @@ function run_tests {
     fi
     python --version
     pwd
-    cd ..
     ls -la
-    ls -la ..
     PSUTIL_TESTING=1 python -Wa ../psutil/psutil/tests/__main__.py
     # PSUTIL_TESTING=1 python -Wa ../psutil/psutil/tests/test_memory_leaks.py
 }
